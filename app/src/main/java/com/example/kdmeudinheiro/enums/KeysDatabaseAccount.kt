ee3957339198d329cbe16f7e6f7147bf9619a5c6
EdsonNeto2533 <edsonneto2533@gmail.com>
2021-08-30 20:03:01 -0300

    ID-64 enum and account model created

---
+++ app/src/main/java/com/example/kdmeudinheiro/enums/KeysDatabaseAccount.kt
@@ -0,0 +1,10 @@
+package com.example.kdmeudinheiro.enums
+
+enum class KeysDatabaseAccount(val key: String, ) {
+    IDACCOUNT("key_account"),
+    IDUSER("ket_user"),
+    PRICE("key_price"),
+    TYPEACCOUNT("key_type"),
+    NAMEACCOUNT("key_name"),
+    EXPIREDATE("key_expiredate")
+}
---
+++ app/src/main/java/com/example/kdmeudinheiro/repository/AccountRepository.kt
@@ -0,0 +1,19 @@
+package com.example.kdmeudinheiro.repository
+
+import com.example.kdmeudinheiro.enums.KeysDatabaseAccount
+import com.example.kdmeudinheiro.model.AccountModel
+import com.google.firebase.firestore.FirebaseFirestore
+
+class AccountRepository {
+    private val db = FirebaseFirestore.getInstance()
+
+    fun addAccount(mAccountModel: AccountModel, callback: (Boolean) -> Unit){
+        val map = mutableMapOf<String , String>()
+        map.put(KeysDatabaseAccount.IDACCOUNT.key, mAccountModel.id_account.toString())
+        map.put(KeysDatabaseAccount.IDUSER.key, mAccountModel.id_user)
+        map.put(KeysDatabaseAccount.PRICE.key, mAccountModel.price.toString())
+        map.put(KeysDatabaseAccount.TYPEACCOUNT.key, mAccountModel.type_account)
+        map.put(KeysDatabaseAccount.NAMEACCOUNT.key, mAccountModel.name_account)
+        map.put(KeysDatabaseAccount.EXPIREDATE.key, mAccountModel.expire_date)
+    }
+}
