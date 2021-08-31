cd839db1fa3d86230e916a1619bb8d799554f410
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-08-31 09:34:45 -0300

    ID-33 Adapter Initial setup

--- app/src/main/java/com/example/kdmeudinheiro/adapter/AdapterBillsList.kt
+++ app/src/main/java/com/example/kdmeudinheiro/adapter/AdapterBillsList.kt
@@ -1,4 +1,23 @@
 package com.example.kdmeudinheiro.adapter
 
-class AdapterBillsList {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kdmeudinheiro.model.BillsModel
+
+class AdapterBillsList(val onItemClick: (BillsModel) -> Unit): RecyclerView.Adapter<BillsViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BillsViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: BillsViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
+class BillsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
 }
