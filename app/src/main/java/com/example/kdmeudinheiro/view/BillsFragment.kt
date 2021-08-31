4717ce9329627194f86ba233fa8c94e98a52e6a3
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-08-31 09:24:34 -0300

    ID-33 Get User session

--- app/src/main/java/com/example/kdmeudinheiro/view/BillsFragment.kt
+++ app/src/main/java/com/example/kdmeudinheiro/view/BillsFragment.kt
@@ -3,17 +3,37 @@ package com.example.kdmeudinheiro.view
 import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.example.kdmeudinheiro.viewModel.BillsViewModel
 import com.example.kdmeudinheiro.R
 import com.example.kdmeudinheiro.databinding.BillsFragmentBinding
+import com.example.kdmeudinheiro.model.BillsModel
+import com.google.firebase.auth.FirebaseUser
 
 class BillsFragment : Fragment(R.layout.bills_fragment) {
+    //var goes here
     private lateinit var viewModel: BillsViewModel
     private lateinit var binding: BillsFragmentBinding
+    //observers gore here
+    private var observerGetBills = Observer<List<BillsModel>> {}
+    private var observerError = Observer<String> {}
+    private var observerAddResponse = Observer<Boolean> {}
+    private var observerUser = Observer<FirebaseUser> { user ->
+        viewModel.getAllBills(user.uid)
+    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         viewModel = ViewModelProvider(this).get(BillsViewModel::class.java)
+        LoadViewModelAndsObservers()
+    }
+
+    private fun LoadViewModelAndsObservers() {
+        viewModel.billList.observe(viewLifecycleOwner, observerGetBills)
+        viewModel.error.observe(viewLifecycleOwner, observerError)
+        viewModel.addResponse.observe(viewLifecycleOwner, observerAddResponse)
+        viewModel.user.observe(viewLifecycleOwner, observerUser)
+        viewModel.getUserId()
     }
 
     companion object {
--- app/src/main/java/com/example/kdmeudinheiro/viewModel/BillsViewModel.kt
+++ app/src/main/java/com/example/kdmeudinheiro/viewModel/BillsViewModel.kt
@@ -5,9 +5,21 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.kdmeudinheiro.model.BillsModel
 import com.example.kdmeudinheiro.repository.BillsRepository
+import com.example.kdmeudinheiro.repository.UserRepository
+import com.google.firebase.auth.FirebaseUser
 
 class BillsViewModel : ViewModel() {
     val billRepo = BillsRepository()
+    val userRepo = UserRepository()
+
+    private val _user = MutableLiveData<FirebaseUser>()
+    var user: LiveData<FirebaseUser> = _user
+
+    fun getUserId() {
+        userRepo.getSession().apply {
+            _user.value = this
+        }
+    }
 
     private val _billList = MutableLiveData<List<BillsModel>>()
     var billList: LiveData<List<BillsModel>> = _billList
@@ -20,11 +32,12 @@ class BillsViewModel : ViewModel() {
             _error.value = e
         }
     }
+
     private val _addResponse = MutableLiveData<Boolean>()
     var addResponse: LiveData<Boolean> = _addResponse
 
-    fun addBill(bill: BillsModel){
-        billRepo.addBills(bill){ resp ->
+    fun addBill(bill: BillsModel) {
+        billRepo.addBills(bill) { resp ->
             _addResponse.value = resp
         }
 
