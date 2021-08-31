47a7278a6eb2f7da84e6ea84a38dea101203a0f8
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-08-31 09:56:23 -0300

    ID-33 Adapter Finished ish

--- app/src/main/java/com/example/kdmeudinheiro/adapter/AdapterBillsList.kt
+++ app/src/main/java/com/example/kdmeudinheiro/adapter/AdapterBillsList.kt
@@ -1,23 +1,38 @@
 package com.example.kdmeudinheiro.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.kdmeudinheiro.R
+import com.example.kdmeudinheiro.databinding.BillItemListBinding
 import com.example.kdmeudinheiro.model.BillsModel
 
 class AdapterBillsList(val onItemClick: (BillsModel) -> Unit): RecyclerView.Adapter<BillsViewHolder>() {
+        private var listOfBills = mutableListOf<BillsModel>()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BillsViewHolder {
-        TODO("Not yet implemented")
+        LayoutInflater.from(parent.context).inflate(R.layout.bill_item_list, parent, false).apply {
+            return BillsViewHolder(this)
+        }
     }
 
     override fun onBindViewHolder(holder: BillsViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        listOfBills[position].apply{
+            holder.bind(this)
+            holder.itemView.setOnClickListener { onItemClick(this) }
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = listOfBills.size
 }
 class BillsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    private var binding = BillItemListBinding.bind(itemView)
 
+    fun bind(bill: BillsModel){
+        binding.textViewBillNameList.text = "${R.string.Bill_Name_label_list_card} ${bill.name_bill}"
+        binding.textViewBillExpireDate.text = "${R.string.Bill_ExpireDate_label_list_card} ${bill.expire_date}"
+        binding.textViewBillPriceList.text = "${R.string.Bill_Price_label_list_card} ${bill.price.toString()}"
+
+    }
 }
