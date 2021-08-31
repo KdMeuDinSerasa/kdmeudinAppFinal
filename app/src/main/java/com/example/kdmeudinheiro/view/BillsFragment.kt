dd9e332a2f9257f12711227052732f7c3f9cf2bc
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-08-31 09:13:52 -0300

    ID-33 Bills Fragment initial setup

--- app/src/main/java/com/example/kdmeudinheiro/view/BillsFragment.kt
+++ app/src/main/java/com/example/kdmeudinheiro/view/BillsFragment.kt
@@ -1,33 +1,22 @@
 package com.example.kdmeudinheiro.view
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProvider
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.kdmeudinheiro.viewModel.BillsViewModel
 import com.example.kdmeudinheiro.R
+import com.example.kdmeudinheiro.databinding.BillsFragmentBinding
 
-class BillsFragment : Fragment() {
+class BillsFragment : Fragment(R.layout.bills_fragment) {
+    private lateinit var viewModel: BillsViewModel
+    private lateinit var binding: BillsFragmentBinding
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        viewModel = ViewModelProvider(this).get(BillsViewModel::class.java)
+    }
 
     companion object {
         fun newInstance() = BillsFragment()
     }
-
-    private lateinit var viewModel: BillsViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.accounts_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(BillsViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 }
