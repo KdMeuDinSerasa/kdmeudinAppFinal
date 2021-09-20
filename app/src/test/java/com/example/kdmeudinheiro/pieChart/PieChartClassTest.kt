9caa92e69ea1b814f42e55c00a248e0cd1e12e04
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-09-20 15:36:18 -0300

    ID-106 making test for piechart

---
+++ app/src/test/java/com/example/kdmeudinheiro/pieChart/PieChartClassTest.kt
@@ -0,0 +1,16 @@
+package com.example.kdmeudinheiro.pieChart
+
+import junit.framework.TestCase
+
+class PieChartClassTest : TestCase() {
+
+    public override fun setUp() {
+        super.setUp()
+    }
+
+    public override fun tearDown() {}
+
+    fun testGetListBills() {}
+
+    fun testSetListBills() {}
+}
