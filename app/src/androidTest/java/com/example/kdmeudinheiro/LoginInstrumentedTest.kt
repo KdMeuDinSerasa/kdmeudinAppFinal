ed54ec0db0298f44d5903c5ef28c9d5ff057de97
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-09-20 15:33:10 -0300

    ID-106 making a instrumented test for login

--- app/src/androidTest/java/com/example/kdmeudinheiro/LoginInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/kdmeudinheiro/LoginInstrumentedTest.kt
@@ -1,4 +1,18 @@
 package com.example.kdmeudinheiro
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.example.kdmeudinheiro.view.LoginActivity
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
 class LoginInstrumentedTest {
+    private lateinit var stringToBeTyped: String
+
+    @get:Rule
+    var activityRule: ActivityScenarioRule<LoginActivity> =
+        ActivityScenarioRule(LoginActivity::class.java)
 }
