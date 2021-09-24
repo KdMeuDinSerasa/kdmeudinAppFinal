6e6922daa4780a5b8107004772f73179312dd9ff
EdsonNeto2533 <edsonneto2533@gmail.com>
2021-09-24 10:26:20 -0300

    ID-130 button ready

--- app/src/main/java/com/example/kdmeudinheiro/viewModel/UserPreferencesViewModel.kt
+++ app/src/main/java/com/example/kdmeudinheiro/viewModel/UserPreferencesViewModel.kt
@@ -1,19 +1,28 @@
 package com.example.kdmeudinheiro.viewModel
 
+import android.content.Context
 import android.net.Uri
+import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.kdmeudinheiro.model.UserModel
+import com.example.kdmeudinheiro.repository.BillsRepository
 import com.example.kdmeudinheiro.repository.UserRepository
+import com.example.kdmeudinheiro.services.NotificationHandler
 import com.google.firebase.auth.FirebaseUser
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
-class UserPreferencesViewModel @Inject constructor(private val mUserRepository: UserRepository) : ViewModel() {
+class UserPreferencesViewModel @Inject constructor(
+    private val mUserRepository: UserRepository,
+    private val mNotificationHandler: NotificationHandler,
+    private val mBillsRepository: BillsRepository
+) : ViewModel() {
 
     private var _mUserModel = MutableLiveData<UserModel>()
     val mUserModel: LiveData<UserModel> = _mUserModel
@@ -28,30 +37,68 @@ class UserPreferencesViewModel @Inject constructor(private val mUserRepository: 
     val imgUser: LiveData<Uri> = _imgUser
 
 
-    fun uploadImgToFirebase(img: String, imgUri: Uri){
-        mUserRepository.uploadImgToFirebase(img, imgUri){ img, error ->
+    fun uploadImgToFirebase(img: String, imgUri: Uri) {
+        mUserRepository.uploadImgToFirebase(img, imgUri) { img, error ->
             if (img != null) _imgUser.value = img
             if (error != null) _error.value = error
         }
     }
 
-    fun userLoged(){
+    fun userLoged() {
         mUserRepository.getSession().apply {
             _mFirebaseUser.value = this
         }
     }
 
-    fun editUser(mUserModel: UserModel){
+    fun editUser(mUserModel: UserModel) {
         viewModelScope.launch {
             mUserRepository.editUser(mUserModel)
         }
 
     }
 
-    fun getUserById(id: String){
+    fun getUserById(id: String) {
         viewModelScope.launch {
             _mUserModel.value = mUserRepository.getUserById(id)
         }
 
+    }
+
+    fun sendNotifications(usedId: String?, acceptNotifications: Boolean, context: Context) {
+        viewModelScope.launch {
+            val calendar = Calendar.getInstance()
+            var count = 0
+            var toExpire = 0
+            val notificationManager = NotificationManagerCompat.from(context)
+            if (acceptNotifications) {
+                if (usedId != null) {
+                    mBillsRepository.getBills(usedId)?.forEach {
+                        if (it.expire_date.after(calendar.time) && it.status == 0) {
+                            count++
+                        } else {
+                            var datePlus = Date();
+                            calendar.setTime(datePlus)
+                            calendar.add(Calendar.DATE, 1)
+                            datePlus = calendar.getTime()
+                            if (it.expire_date.before(datePlus) && it.status == 0) toExpire++
+                        }
+                    }
+                    if (count > 0) mNotificationHandler.createNotification(
+                        "Você possui contas vencidas",
+                        "Total de contas vencidas: $count"
+                    ).apply {
+                        notificationManager.notify(1, this)
+
+                    } else if (toExpire > 0) {
+                        mNotificationHandler.createNotification(
+                            "Você possui há vencer",
+                            "Total de contas há vencer: $toExpire"
+                        ).apply {
+                            notificationManager.notify(1, this)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
