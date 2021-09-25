9d9ef1be4e4d6fcfb645389693f7b5e7320b9fbc
EdsonNeto2533 <edsonneto2533@gmail.com>
2021-09-25 10:04:36 -0300

    ID-133 layout created

---
+++ app/src/main/java/com/example/kdmeudinheiro/view/ForgetPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kdmeudinheiro.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kdmeudinheiro.R
+
+class ForgetPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forget_password)
+    }
+}
