63d3bfbe5bf6638ebaf0424ab764e7483549d4ce
EdsonNeto2533 <edsonneto2533@gmail.com>
2021-09-28 11:54:23 -0300

    ID-144 tested

--- app/src/main/java/com/example/kdmeudinheiro/view/ForgetPasswordActivity.kt
+++ app/src/main/java/com/example/kdmeudinheiro/view/ForgetPasswordActivity.kt
@@ -3,11 +3,13 @@ package com.example.kdmeudinheiro.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.ui.AppBarConfiguration
 import com.example.kdmeudinheiro.R
 import com.example.kdmeudinheiro.databinding.ActivityForgetPasswordBinding
 import com.example.kdmeudinheiro.utils.feedback
+import com.example.kdmeudinheiro.utils.hideKeyboard
 import com.example.kdmeudinheiro.viewModel.ForgetPasswordViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -30,6 +32,7 @@ class ForgetPasswordActivity : AppCompatActivity() {
         return true
     }
 
+
     fun loadViewModels(){
         viewModel.success.observe(this, {
             if (it) feedback(binding.root, R.string.redefinition_email_send, R.color.success)
@@ -42,6 +45,14 @@ class ForgetPasswordActivity : AppCompatActivity() {
     fun loadComponents(){
         binding.btnSendEmail.setOnClickListener {
             viewModel.sendRedefinitionEmail(binding.tvEmail.text.toString())
+        }
+
+        binding.tvEmail.setOnKeyListener { view, keyCode, keyEvent ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && keyEvent.action == KeyEvent.ACTION_UP){
+                hideKeyboard()
+                return@setOnKeyListener true
+            }
+            return@setOnKeyListener false
         }
     }
 }
