865f52ab36535f8d1e615b6ea9645c0a95773066
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2022-04-07 11:31:38 -0300

    refac[WIMMY-10]: created more functions to separate responsibility

--- app/src/main/java/com/example/kdmeudinheiro/view/SplashActivity.kt
+++ app/src/main/java/com/example/kdmeudinheiro/view/SplashActivity.kt
@@ -6,16 +6,14 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.edit
 import androidx.lifecycle.ViewModelProvider
 import com.example.kdmeudinheiro.databinding.ActivitySplashBinding
 import com.example.kdmeudinheiro.enums.KeysShared
-import com.example.kdmeudinheiro.services.NotificationHandler
 import com.example.kdmeudinheiro.viewModel.LoginViewModel
 import com.example.kdmeudinheiro.viewModel.MainViewModel
+import com.google.firebase.auth.FirebaseUser
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
@@ -39,32 +37,36 @@ class SplashActivity : AppCompatActivity() {
             viewModelLogin.checkSession()
         } else
             Handler().postDelayed({
-                startActivity(Intent(this, LoginActivity::class.java))
-                finish()
+                startLoginActivity()
             }, 3000)
 
 
     }
 
-    fun loadViewModels() {
-        viewModelLogin.mFirebaseUser.observe(this, {
-            if (it != null) {
-                mSharedPreferences.edit {
-                    this.putString(KeysShared.USERID.key, it.uid)
-                }
-                viewModelMain.getUserById(it.uid)
-                Handler().postDelayed({
-                    viewModelMain.getIncome(it.uid)
-                }, 3000)
-            } else {
-                startActivity(Intent(this, LoginActivity::class.java))
-                finish()
-            }
-
-        })
+   private fun loadViewModelsObservers() {
+        viewModelLogin.mFirebaseUser.observe(this, { handlerUserInstance(it) })
         viewModelMain.mIncomeModel.observe(this, {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         })
+    }
+
+    private fun handlerUserInstance(userReceived: FirebaseUser?){
+        if (userReceived != null) {
+            mSharedPreferences.edit {
+                this.putString(KeysShared.USERID.key, userReceived.uid)
+            }
+            viewModelMain.getUserById(userReceived.uid)
+            Handler().postDelayed({
+                viewModelMain.getIncome(userReceived.uid)
+            }, 3000)
+        } else {
+            startLoginActivity()
+        }
+    }
+
+    private fun startLoginActivity(){
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
     }
 }
